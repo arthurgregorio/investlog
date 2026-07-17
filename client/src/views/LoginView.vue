b6faff0fd094fd7c8f7cecc65978ecfe543215d4
Arthur Gregorio <arthurshakal@gmail.com>
2026-07-16 23:34:20 -0300

    fixing css

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -218,7 +218,7 @@ async function submitTotpCode() {
             expanded
             native-type="submit"
             :loading="submitting"
-            class="auth-submit"
+            class="auth-submit has-text-light"
           >
             Criar conta
           </b-button>
